3d4b9290c5c2120beda7fce40230bbbee70fdebe
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-23 14:08:20 +0430

    Initial Repo

---
+++ StackWithArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using StackWithArray;
+
+var stack = new Stack(4);
+
+stack.Push(12);
+stack.Push(3);
+stack.Push(2);
+stack.Push(90);
+stack.Push(90);
+
+stack.Pop();
+stack.Pop();
+
+stack.Print();
---
+++ StackWithArray/Stack.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackWithArray
+{
+    public class Stack
+    {
+        public int size;
+        public int top;
+        public int[] stack;
+
+        public Stack(int sizeOfStack)
+        {
+            size = sizeOfStack;
+            top = -1;
+            stack = new int[size];
+        }
+
+        public void Push(int item)
+        {
+            if (IsFull())
+            {
+                Console.WriteLine("stack is full");
+            }
+
+            else
+            {
+                top++;
+                stack[top] = item;
+            }
+        }
+
+        public void Pop()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("the stack is empty");
+            }
+            else
+            {
+                int result = stack[top];
+                Console.WriteLine(result);
+                top--;
+            }
+        }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("the stack is empty!");
+            }
+            else
+            {
+                for (int i = 0; i <= top; i++)
+                {
+                    Console.WriteLine(stack[i]);
+                }
+            }
+
+        }
+
+        private bool IsFull()
+        {
+            if (top == (size - 1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool IsEmpty()
+        {
+            if (top == -1)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
